4b098040af91ea241ad3482c554cf4a023335682
Israel Dixon <77282940+IsraelXDixon@users.noreply.github.com>
2021-06-25 06:51:00 -0400

    Add files via upload

---
+++ Controllers/ExamController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KnowItAlls.Models;
+
+namespace KnowItAlls.Controllers
+{
+    public class ExamController : Controller
+    {
+        private readonly AppDbContext _abd;
+
+        public ExamController(AppDbContext abd)
+        {
+            _abd = abd;
+        }
+        public IActionResult Index()
+        {
+            var results = _abd.onlineexam.ToList();
+            return View(results);
+        }
+    }
+}
---
+++ OnlineExam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace KnowItAlls.Models
+{
+    public class OnlineExam
+    {
+        [Key]
+        public int Qid {get; set;}
+        public string Question { get; set; }
+        public string Option1 { get; set; }
+        public string Option2 { get; set; }
+        public string Option3 { get; set; }
+        public string Option4 { get; set; }
+
+        public string Correctans { get; set; }
+
+
+    }
+}
